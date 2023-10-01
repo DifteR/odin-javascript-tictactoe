17042d63c8faa4e89651381ccd59d9a93cad55f9
Tilen Marš <its0black0magic@gmail.com>
2023-10-01 13:37:24 +0000

    Create play method

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,26 @@
 var gameContainer = document.getElementById("game-container");
 
-function playerFactory(name, playingSymbol){
+function playerFactory(name, playingSymbol) {
     this.name = name;
     this.playingSymbol = playingSymbol;
-    return{
-
+    return {
+        name,
+        playingSymbol
     }
 }
+
+gameBoard = function () {
+    var gameArray = new Array(3);
+    for (var i = 0; i < 3; i++) {
+        gameArray[i] = new Array(3);
+    }
+
+    function play(x,y,playingSymbol) {
+        gameArray[x,y] = playingSymbol;
+        console.table(gameArray);
+    }
+
+}()
+
+playerFactory(Tilen, -1);
+gameBoard.play(0,0,-1);

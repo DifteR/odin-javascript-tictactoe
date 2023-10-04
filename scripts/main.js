c9cd1d74814e7ccd4e5d05ce1e38ddca3e5a0c6b
Tilen Marš <its0black0magic@gmail.com>
2023-10-04 15:46:36 +0000

    Create method to draw a grid for the game

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,3 @@
-var gameContainer = document.getElementById("game-container");
-
 function playerFactory(name, playingSymbol) {
     this.name = name;
     this.playingSymbol = playingSymbol;
@@ -15,20 +13,34 @@ gameBoard = function () {
         gameArray[i] = new Array(3);
     }
 
-    function play(x,y,playingSymbol) {
+    function play(x, y, playingSymbol) {
         if (!gameArray[x][y]) {
             gameArray[x][y] = playingSymbol;
             console.table(gameArray);
         }
-        else(
-            console.log("Failed!")
-        )
+        else {
+            console.log("failed");
+        }
     }
-    return{
-        play
+    function drawGame() {
+        let gameTable = document.getElementById("game-table");
+        console.log(gameTable);
+        for (let i = 0; i < gameArray.length; i++) {
+            let row = gameTable.insertRow(i);
+            console.log("I'm a row");
+            for (let j = 0; j < gameArray.length; j++) {
+                let cell = row.insertCell(j);
+                cell.innerText = " ";
+            }
+        }
+    }
+    return {
+        play,
+        drawGame //must remove, just for testing
     }
 }()
 
 playerFactory("Tilen", -1);
-gameBoard.play(0,0,-1);
-gameBoard.play(0,0,-1);
+gameBoard.play(0, 0, -1);
+gameBoard.play(0, 0, -1);
+gameBoard.drawGame();
